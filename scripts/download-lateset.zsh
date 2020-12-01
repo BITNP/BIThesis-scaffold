6df2dba289fc8cd8fcd26fcf46652f60c5272ce3
fengkaiyu <loveress01@outlook.com>
2020-12-01 18:34:32 +0800

    feat(test): add script for diff testing.
    This zsh script will automatically build diff test
    between latest version and local dev version.

---
+++ scripts/download-lateset.zsh
@@ -0,0 +1,59 @@
+set -e
+REPO=BITNP/BIThesis
+
+CACHE_FILE=latest.json
+
+CACHE_CUR_VERSION_FILE=cache_current_version.txt
+
+# download latest files if not cached
+
+curl -L -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$REPO/releases/latest > $CACHE_FILE
+
+print "get current version"
+
+if [[ -f "$CACHE_CUR_VERSION_FILE" ]] {
+  CUR_VERSION=`cat $CACHE_CUR_VERSION_FILE`
+} else {
+  CUR_VERSION=""
+}
+
+print "get current cache"
+ZIP_URL=`cat $CACHE_FILE | jq -r '.assets|.[] | {browser_download_url} | .browser_download_url'`
+VERSION=`cat $CACHE_FILE | jq -r '.tag_name'`
+
+ZIP_URL=`echo $ZIP_URL | tr '\n' ' '`
+ZIP_URL=(${=ZIP_URL})
+
+if [[ $VERSION != $CUR_VERSION ]] {
+  print try download latest version: $VERSION.
+  print '\n'
+  print -l $ZIP_URL
+
+  for i ($ZIP_URL) {
+    if [[ $i == *.zip ]] {
+      wget $i
+    }
+  }
+  echo $VERSION > $CACHE_CUR_VERSION_FILE
+} else {
+  echo $VERSION already downloaded.
+}
+
+ORI_PWD=`pwd`
+echo $PWD
+
+for i ($ZIP_URL){
+  if [[ $i == *.zip ]] {
+    file_name=${i:t}
+    unzip -o $file_name
+    dir_name=${file_name%.zip}
+    cd $dir_name
+    latexmk -synctex=1 -interaction=nonstopmode -file-line-error -xelatex main.tex
+    cd ../
+    cd ../$dir_name
+    latexmk -synctex=1 -interaction=nonstopmode -file-line-error -xelatex main.tex
+    cd $ORI_PWD
+    diff-pdf --view ./$dir_name/main.pdf ../$dir_name/main.pdf
+  }
+}
